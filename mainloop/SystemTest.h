204a6b39d414a9d3625807baa3c9646d0c2a46c5
Félix <felixcantet@gmail.com>
2021-03-30 12:50:29 +0200

    Add test system

--- mainloop/SystemTest.h
+++ mainloop/SystemTest.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "EcsBase.h"
 #include "SystemBase.h"
+#include "JobSystem.h";
+#include <iostream>
 namespace Tests
 {
 	class SystemTest : ECS::SystemBase
 	{
 		virtual void Update()
 		{
-
+			JobSystem::Execute(
+				[]() {
+					std::cout << "System " << typeid(this).name() << " Executed";
+				}
+			);
 		}
 	};
 }
